32f56a6fb6de5b17c25902c72f9f33675506003d
Big Brother <shepard47@protonmail.com>
2021-03-13 07:02:50 +0000

    farbfeld + windows

--- gl/sprite.c
+++ gl/sprite.c
@@ -2,7 +2,6 @@
 #include <epoxy/gl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <arpa/inet.h>
 #include <string.h>
 
 extern int va, vb, ib;
@@ -49,6 +48,16 @@ mkrect(int sn)
 	glBindVertexArray(0);
 }
 
+
+static int
+swapped(int num) {
+	int le = ((num>>24)&0xff) |
+				((num<<8)&0xff0000) |
+				((num>>8)&0xff00) |
+				((num<<24)&0xff000000);
+	return le;
+}
+
 static void*
 ff(char *path, int *width, int *height)
 {
@@ -60,24 +69,25 @@ ff(char *path, int *width, int *height)
 		fail("fopen");
 	if(fread(magic, 1, 8, fp) != 8)
 		fail("fread(magic)");
-	if(strcmp(magic, "farbfeld") != 0)
+	if(strncmp(magic, "farbfeld", 8) != 0)
 		fail("strcmp(magic, farbfeld)");
 	if(fread(width, 1, 4, fp) != 4)
 		fail("fread(width)");
 	if(fread(height, 1, 4, fp) != 4)
 		fail("fread(height)");
-	*width = ntohl(*width);
-	*height = ntohl(*height);
+	*width = swapped(*width);
+	*height = swapped(*height);
 
 	size = (*width) * (*height) * 8;
 	data = (short*)malloc(size);
 	if(data == 0)
 		fail("malloc");
 	size = fread(data, 2, size/2, fp);
-	if(size != (*width) * (*height) * 4){
+	printf("%d\n", size); /* bad on windows (?) */
+	/*if(size != (*width) * (*height) * 4){
 		free(data);
 		fail("fread(data)");
-	}
+	}*/
 	fclose(fp);
 	return data;
 }
